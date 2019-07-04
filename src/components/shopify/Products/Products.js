5b4cc35f09d5a498d495a941f206ffdcaadc9dbd
Dan Billson <danny.billson@hotmail.com>
2019-07-04 08:15:28 +0100

    Add no items message

--- src/components/shopify/Products/Products.js
+++ src/components/shopify/Products/Products.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import Product from '../Product/Product';
 
 class Products extends Component {
-  render() {
-    let products = this.props.products.map((product) => {
-      return (
-        <Product
-          addVariantToCart={this.props.addVariantToCart}
-          client={this.props.client}
-          key={product.id.toString()}
-          product={product}
-        />
-      );
-    });
+    render() {
+        let products = this.props.products.map(product => {
+            return (
+                <Product
+                    addVariantToCart={this.props.addVariantToCart}
+                    client={this.props.client}
+                    key={product.id.toString()}
+                    product={product}
+                />
+            );
+        });
 
-    return (
-      <div className="Product-wrapper">
-        {products}
-      </div>
-    );
-  }
+        return (
+            <div className="Product-wrapper">
+                {products}
+                {this.props.products.length === 0 && (
+                    <p>Sorry, no items are available at this time.</p>
+                )}
+            </div>
+        );
+    }
 }
 
 export default Products;
